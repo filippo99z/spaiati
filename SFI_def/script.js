117237d6bf33d0bc65e3bf492fd7db49259b7e2c
FilippoZuccotti <filippo.zucco23@gmail.com>
2024-03-20 14:39:38 +0100

    merge trivia

---
+++ SFI_def/script.js
@@ -0,0 +1,16 @@
+function gira(element){
+    let counter = document.getElementById("counter").textContent;
+    console.log(counter);
+    if(counter<3){
+        counter++;
+
+        document.getElementById("counter").innerHTML = counter;
+        element.style.display ="none";
+        let fronte = element.nextElementSibling;
+        fronte.style.display ="block";
+    }else{
+        console.log("non puoi girare più carte");
+    }
+
+
+}
