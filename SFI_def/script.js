249159793c6dce26f588a253c0f07b51472a42f8
FilippoZuccotti <filippo.zucco23@gmail.com>
2024-03-25 16:41:15 +0100

    gallery

--- SFI_def/script.js
+++ SFI_def/script.js
@@ -1,3 +1,5 @@
+var counter =0;
+var counterSm = 0;
 function gira(element){
     let counter = document.getElementById("counter").textContent;
     console.log(counter);
@@ -14,7 +16,92 @@ function gira(element){
 
 
 }
-function slide() {
-    let img =document.getElementById("roll1");
-    img.classList.add("roll-out-left");
+function slideOut() {
+    console.log("cliccato");
+    if(counter<7){
+    counter++;
+    console.log("slide out"+counter);
+    let idPhoto = "roll"+counter
+    let img =document.getElementById(idPhoto);
+
+        if (counter%2 ==0){
+            img.classList.remove("roll-in-left");
+            img.classList.add("roll-out-left");
+            setTimeout(function() {
+                img.style.display = "none";
+            }, 600);
+        }else{
+            img.classList.remove("roll-in-right");
+            img.classList.add("roll-out-right");
+            setTimeout(function() {
+                img.style.display = "none";
+            }, 600);
+        }
+    }
+
+}
+function slideIn() {
+    if (counter>0) {
+        console.log("slide in"+counter);
+
+        let idPhoto = "roll" + counter
+        let img = document.getElementById(idPhoto);
+
+        if (counter % 2 == 0) {
+            img.classList.add("roll-in-left");
+            img.style.display = "block";
+
+
+        } else {
+            img.classList.add("roll-in-right");
+            img.style.display = "block";
+
+        }
+        counter--;
+    }
+}
+
+function slideOutSm() {
+    console.log("cliccato");
+    if(counterSm<7){
+        counterSm++;
+        console.log("slide out"+counter);
+        let idPhoto = "rollsm"+counterSm
+        let img =document.getElementById(idPhoto);
+
+        if (counterSm%2 ==0){
+            img.classList.remove("roll-in-left");
+            img.classList.add("roll-out-left");
+            setTimeout(function() {
+                img.style.display = "none";
+            }, 600);
+        }else{
+            img.classList.remove("roll-in-right");
+            img.classList.add("roll-out-right");
+            setTimeout(function() {
+                img.style.display = "none";
+            }, 600);
+        }
+    }
+
+}
+function slideInSm() {
+    if (counterSm>0) {
+        console.log("slide in"+counterSm);
+
+        let idPhoto = "rollsm" + counterSm
+        let img = document.getElementById(idPhoto);
+
+        if (counterSm % 2 == 0) {
+            img.classList.add("roll-in-left");
+            img.style.display = "block";
+
+
+        } else {
+            img.classList.add("roll-in-right");
+            img.style.display = "block";
+
+        }
+        counterSm--;
+    }
 }
